a7a6b7bc84c5e0c6fee6cf1aadd11a4b399d9726
jian-level-is <myunggeun22@gmail.com>
2021-06-28 14:39:22 +0900

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,12 +1,41 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 
-let paintingStatus = "false";
+let paintingStatus = false;
 
 canvas.Width = 800;
 canvas.height= 800;
 ctx.strokeStyle = "black";
 ctx.lineWidth = "2.5";
+
+
+//handle touch
+let touchMode = false;
+
+canvas.addEventListener("touchstart",touchstart);
+canvas.addEventListener("touchmove", touching);
+canvas.addEventListener("touchend",touchEnd);
+//start touch mode
+function touchstart(){
+    touchMode = true;
+    ctx.beginPath();    
+}
+
+function touching(event){
+    event.preventDefault();
+
+    const x = event.changedTouches[0].clientX; 
+    const y = event.changedTouches[0].clientY;
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    console.log();
+}
+
+function touchEnd(event){
+    event.preventDefault();
+    ctx.closePath();
+    touchMode = false;
+}
 
 
 //Drowing event
@@ -47,4 +76,13 @@ const fill = document.querySelector("#jsMode");
 fill.addEventListener("click", fillTheBg);
 function fillTheBg(){
     ctx.fillRect(0,0,canvas.Width,canvas.height);
+}
+
+//change brush
+const brush = document.querySelector("#jsRange");
+
+brush.addEventListener("input", handleRange);
+
+function handleRange(event){
+    ctx.lineWidth = event.target.value; 
 }
